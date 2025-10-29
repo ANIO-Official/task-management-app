cf4e007767dd5edf9e5fdd8d251ee911151a5477
Amanda Ogletree <amandaogletree19@gmail.com>
2025-10-29 10:18:36 -0400

    Reformated  javascript code. Uncommented out DOM Manipulation variables and values. Created new function addTask() with parameter task to create an push a new task to the tasks array.

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
 /////Global Variables/////
 //Tasks Array
 let tasks = []
+
 //New Task Specific Variables
-// let taskName = document.querySelector("#taskName")
-// let category = document.querySelector('input[name="category":checked')
-// let deadline = document.querySelector("#deadline")
-// let status = document.querySelector('.statusCheckbox:checked')
+let taskName = document.querySelector("#taskName")
+let category = document.querySelector('input[name="category":checked')
+let deadline = document.querySelector("#deadline")
+let status = document.querySelector('.statusCheckbox:checked')
+
+//////////////////////////////
+//      Data Storage        //
+//////////////////////////////
 
 //Creates a new task object, to be called on addTaskBtn EventListener
-//Data only for Array
+//Data only for Array | Stores Task into an Object
 function createTask(taskName, category, deadline, status){
     let newTask = {
-        taskName: taskName,//.value,
-        category: category,//.innerText,
-        deadline: deadline,//.value,
-        status: status//.innerText,
+        taskName: taskName.value,
+        category: category.innerText,
+        deadline: deadline.value,
+        status: status.innerText,
     }
     return newTask
 }
 
-//test to see if createTask function works in console.
+//Adds task to array
+//task parameter represents all new task
+function addTask(task){
+    /*set's the createTask()'s parameter to the input values from HTML 
+    and places them into the variable parameter task to then push into the array*/
+    task = createTask(taskName, category, deadline, status)
+    tasks.push(task)
+}
 
-let task = createTask("John's Birthday", "Family", "05/22/2026", "On Hold")
-//console.log(task)
+//////////////////////////////
+//      Display Tasks       //
+//////////////////////////////
 
-//Test pushing it into array
-tasks.push(task)
-console.log(tasks)
+
+
+
+
+
+
+// //test to see if createTask function works in console.
+
+// let task1 = createTask("John's Birthday", "Family", "05/22/2026", "On Hold")
+// //console.log(task)
+
+// //Test pushing it into array
+// tasks.push(task)
+// console.log(tasks)
